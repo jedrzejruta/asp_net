80b31c2774b64e37a2a27791b84d4d9f1a7d04d1
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-12-18 16:26:09 +0100

    Seed data with local admin credentials to check login procedure

---
+++ lab2/Models/IdentidySeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace lab2.Models
+{
+				public class IdentidySeedData
+				{
+								private const string login = "Admin"; // use of const instead of readonly? will we change this values later?
+								private const string password = "Admin123$";
+
+								public static async Task EnsurePopulated(UserManager<IdentityUser> userManager)
+								{
+												IdentityUser user = await userManager.FindByIdAsync(login);
+												if(user == null)
+												{
+																user = new IdentityUser("Admin");
+																await userManager.CreateAsync(user, password);
+												}
+								}
+				}
+}
