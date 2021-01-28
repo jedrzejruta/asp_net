ee76854af60ccfcb6ec09c20f3a45d9cd369cca3
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2021-01-28 01:22:02 +0100

    add walidation to Product class

--- lab2/Models/Product.cs
+++ lab2/Models/Product.cs
@@ -12,15 +12,20 @@ namespace lab2.Models
 								public int Id { get; set; }
 
         [Display(Name = "Nazwa")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Opis")]
+        [Required]
         public string Description { get; set; }
 
         [Display(Name = "Cena")]
+        [Required]
+        [Range(0.01, int.MaxValue)]
         public decimal Price { get; set; }
 
         [Display(Name = "Kategoria")]
+        [Required]
         public string Category { get; set; }
     }
 }
